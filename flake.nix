7f018c778b5b1100aa030cebe86084af38a31d01
Daniel Shiposha <dev@shiposha.com>
2025-06-16 11:47:30 +0200

    feat: use regreet 0.2.0

--- flake.nix
+++ flake.nix
@@ -1,28 +1,26 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    nixosModules = {
-      poly = import ./modules/poly.nix;
-      greetd = import ./modules/greetd.nix;
-      regreet = import ./modules/regreet.nix;
-      multiseat = import ./modules/multiseat.nix;
-      shared = import ./modules/shared.nix;
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosModules = {
+        poly = import ./modules/poly.nix;
+        greetd = import ./modules/greetd.nix;
+        regreet = import ./modules/regreet.nix;
+        multiseat = import ./modules/multiseat.nix;
+        shared = import ./modules/shared.nix;
+      };
+      packages.${system} = {
+        greetd = pkgs.greetd.greetd.overrideAttrs (old: {
+          patches = old.patches ++ [ ./patches/greetd-multiseat-support.patch ];
+        });
+        regreet = pkgs.greetd.regreet.overrideAttrs (old: {
+          patches = old.patches
+            ++ [ ./patches/regreet-multiseat-support.patch ];
+        });
+      };
     };
-    packages.${system} = {
-      greetd = pkgs.greetd.greetd.overrideAttrs (old: {
-        patches = old.patches ++ [ ./patches/greetd-multiseat-support.patch ];
-      });
-      regreet = pkgs.greetd.regreet.overrideAttrs (old: {
-        version = "0.1.2";
-        patches = old.patches ++ [ ./patches/regreet-multiseat-support.patch ];
-      });
-    };
-  };
 }
